b6b7cf3b858e536a172a91c5445a1957922d16cf
Daniel <jkldaniel@gmail.com>
2022-09-27 21:13:29 -0300

    aula nova

---
+++ AV1_AULA4/ex3.py
@@ -0,0 +1,9 @@
+#Faça um Programa que peça dois números e imprima a soma.
+
+def sum(num1, num2):
+    return num1 + num2
+    
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro número: "))
+
+print ("O resultado da soma é %d" %sum(num1, num2))
---
+++ AV1_AULA4/ex9.py
@@ -0,0 +1,6 @@
+#Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
+
+
+F = float(input('Temperatura em Fahrenheit: '))
+
+print ('Temperatura em Celsius: %.2f' %(5*((F-32)/9)))
