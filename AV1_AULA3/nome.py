79fe84fcec4e14856f06a3506e66030c60b324c6
Daniel <jkldaniel@gmail.com>
2022-09-27 22:13:04 -0300

    trabalho

--- AV1_AULA3/nome.py
+++ AV1_AULA3/nome.py
@@ -1,5 +1,7 @@
 from tkinter import N
 
+#Faça um programa que exiba o seu nome, sua matrícula e seu curso.
+
 print('Olá, meu nome é Daniel Soares Oliveira da Silva. \n')
 print('Minha matrícula é 202102410241. \n')
 print('Meu curso é Gestão de TI. \n')
